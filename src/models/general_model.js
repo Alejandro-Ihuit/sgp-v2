ea3b79b2f0f3beb0c5b7778c8176d3317ef0aa0e
Alejandro de Jesus Ihuit Herrera <alex_12ajih@hotmail.com>
2023-05-29 17:40:32 -0600

    se crea la funcion y la query para insertar

--- src/models/general_model.js
+++ src/models/general_model.js
@@ -1,4 +1,4 @@
-import { select } from "../connections/base125.js";
+import { select, insert } from "../connections/base125.js";
 import { select210 } from "../connections/base210.js";
 
 const escuelasQuery = async() => {
@@ -13,7 +13,19 @@ const campanias = async() => {
     return await select210(query);
 }
 
+const insertProspecto = async(prospecto) => { //fala agregar los try cath
+
+    let query = `INSERT INTO sgp.prospectos
+    (primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, fecha_nacimiento, curp, rfc, celular, dir_casa, codigo_postal, cc_place, asesor_padrino, campania, turno, modalidad, horas_laborales, bono, fecha_ingreso, escuela, medios, reclutador, fecha_solicitud, estatus)
+    VALUES('${prospecto.primer_nombre}', '${prospecto.segundo_nombre}', '${prospecto.primer_apellido}', '${prospecto.segundo_apellido}', '${prospecto.fecha_nacimiento}', '${prospecto.curp}', '${prospecto.rfc}', '${prospecto.celular}', '${prospecto.dir_casa}', '${prospecto.codigo_postal}', '${prospecto.cc_experience}', '${prospecto.asesor_padrino}', '${prospecto.campania}', '${prospecto.turno}', '${prospecto.modalidad}', ${prospecto.horas_laborales}, '${prospecto.bono}', '${prospecto.fecha_ingreso}', '${prospecto.escuela}', '${prospecto.medios}', '${prospecto.reclutador}', '${prospecto.fecha_solicitud}', ${prospecto.estatus});
+    `;
+
+    return await insert(query);
+
+}
+
 export {
     escuelasQuery,
-    campanias
+    campanias,
+    insertProspecto
 }
